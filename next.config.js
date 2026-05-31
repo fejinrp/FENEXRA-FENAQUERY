889783f29d0df550abd304596f292eb9d4309dfb
Fejin RP <fejinrp@gmail.com>
2026-05-31 12:42:37 +0530

    chore: remove basePath and assetPrefix configuration from Next.js settings

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: '/apps/fenaquery',
-  assetPrefix: '/apps/fenaquery'
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
